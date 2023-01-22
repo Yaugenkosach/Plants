00a045d894481159258be7378b224196f8b5473c
yaugenkosach <yaugenkosach@gmail.com>
2023-01-22 16:19:12 +0300

    feat: add mobile adaptive for burger-menu

--- plants/index.js
+++ plants/index.js
@@ -4,10 +4,18 @@
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header-nav');
     const menuCloseItem = document.querySelector('.header-nav-close');
+    const menuLinks = document.querySelectorAll('.header-link');
     burgerItem.addEventListener('click', () => {
         menu.classList.add('header-nav-active');
     });
     menuCloseItem.addEventListener('click', () => {
         menu.classList.remove('header-nav-active');
     })
+    if(window.innerWidth <= 767) {
+        for (let i = 0; i < menuLinks.length; i += 1) {
+            menuLinks[i].addEventListener('click', () => {
+                menu.classList.remove('header-nav-active');
+            });
+        }
+    }
 }());
